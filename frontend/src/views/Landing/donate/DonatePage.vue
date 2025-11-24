8c234db251411a87f4fdb30657eb5ae20786408a
michelmlg <147891522+michelmlg@users.noreply.github.com>
2025-11-24 00:55:24 -0300

    fix: build error with image

--- frontend/src/views/Landing/donate/DonatePage.vue
+++ frontend/src/views/Landing/donate/DonatePage.vue
@@ -32,7 +32,7 @@
                 <div class="w-64 h-64 bg-white rounded-xl flex items-center justify-center shadow-md">
                 <template v-if="qrImageUrl">
                     <img
-                        :src="qrImageUrl"
+                        src="../../../../public/images/QR-payment.jpg"
                         alt="QR Code para doação (PIX)"
                         class="object-contain w-full h-full"
                         @error="onImgError"
